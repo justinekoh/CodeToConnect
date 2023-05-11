de09621d399cc95148534e59aa014c070e3639ed
justinek <justinetaikoh@gmail.com>
2023-05-11 10:28:19 +0800

    Initialize projects

---
+++ codetoconnect/client_configuration_tool/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ClientConfigurationToolConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'client_configuration_tool'
