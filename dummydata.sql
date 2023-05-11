0af2479a0cf79bfa98f664a2f559c4d3dde59433
Neo Jing Xuan <neojxuan@gmail.com>
2023-05-11 16:44:49 +0800

    add dummydata sql

---
+++ dummydata.sql
@@ -0,0 +1,11 @@
+INSERT INTO client_configuration_tool_roles (roleId, name)
+VALUES (1, "Role1");
+
+INSERT INTO client_configuration_tool_roles (roleId, name)
+VALUES (2, "Role2");
+
+INSERT INTO client_configuration_tool_users (id, name, roleId_id)
+VALUES (1, "User1", 1);
+
+INSERT INTO client_configuration_tool_users (id, name, roleId_id)
+VALUES (2, "User2", 2);
